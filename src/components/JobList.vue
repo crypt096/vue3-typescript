693c7fb4702e498a79b47922f57fed0727121c79
crypt0 <acvjetan@gmail.com>
2021-05-06 11:58:48 +0200

    Make JobList with more details

--- src/components/JobList.vue
+++ src/components/JobList.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="job-list">
     <ul>
-      <li v-for="job in jobs" :key="job.id">{{ job.title }}</li>
+      <li v-for="job in jobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum
+            ratione animi reiciendis? Earum nemo, aliquam magnam excepturi error
+            molestias incidunt laudantium. Explicabo harum, quam debitis sunt
+            itaque voluptate dolore asperiores.
+          </p>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
