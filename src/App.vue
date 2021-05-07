233ed8b166309dfef8ab4c51c44b42b3cd2efa18
crypt0 <acvjetan@gmail.com>
2021-05-07 11:20:38 +0200

    Add styling.

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="app">
-    <button @click="handleClick('title')">Order by title</button>
-    <button @click="handleClick('salary')">Order by salary</button>
-    <button @click="handleClick('location')">Order by location</button>
+    <header>
+      <div class="order">
+        <button @click="handleClick('title')">Order by title</button>
+        <button @click="handleClick('salary')">Order by salary</button>
+        <button @click="handleClick('location')">Order by location</button>
+      </div>
+    </header>
+
     <JobList :jobs="jobs" :order="order" />
   </div>
 </template>
@@ -63,5 +68,21 @@ export default defineComponent({
 </script>
 
 <style>
+header {
+  text-align: center;
+}
+header .order {
+  margin-top: 20px;
+}
+button {
+  margin: 0 10px;
+  color: #1195c9;
+  border: 3px solid #1195c9;
+  background: #d5f0ff;
+  padding: 8px 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-weight: bold;
+}
 </style>
  
